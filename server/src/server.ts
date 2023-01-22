9d752361e0ebf4ac61dd3a40a61314f305892abf
Rudnei Santiago <rudnei.santiago@gmail.com>
2023-01-22 16:55:35 -0300

    add axios, configure dayjs and allow localhost to cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,23 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import { appRoutes } from './lib/routes'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./lib/routes";
 
-const app = Fastify()
+const app = Fastify();
 
 app.register(cors, {
-  origin: ['http://localhost:3000']
-})
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+  ],
+});
 
-app.register(appRoutes)
+app.register(appRoutes);
 
 app
   .listen({
-    port: 3333
+    port: 3333,
   })
   .then(() => {
-    console.log('HTTP Server running!')
-  })
+    console.log("HTTP Server running!");
+  });
